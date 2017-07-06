f47952bccf87232c6de00e61e16a840220c8d445
V-Venkatesh <v-venchu@microsoft.com>
2017-07-06 11:09:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
-#!bin/bash
-mkdir tempCore
+#!/bin/bash
+[ -d tempCore ] || mkdir tempCore
 cd tempCore
 wget https://raw.githubusercontent.com/V-Venkatesh/coreos-install/master/cloud-config-file.yml
 ls
-read -p " Press Yes[Y] to continue coreos installation into machine, No[N] to exit " flag
-if [ $flag -eq Y or $flag -eq y ]
+echo " Press Yes[Y] to continue coreos installation into machine, No[N] to exit " flag
+# to read strings
+read flag
+if (( (( $flag == "Y")) || (($flag == "y")) )) ]
 then
   echo "Starting coreos installation proccess"
-  coreos-install -d dev/sda -C stable -c cloud-config-file.yml
+  sudo coreos-install -d /dev/sda -C stable -c cloud-config-file.yml
 fi
+  
