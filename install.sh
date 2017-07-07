a3356498e5f2693d9d36c17b38885aa5edaf43a9
V-Venkatesh <v-venchu@microsoft.com>
2017-07-07 10:24:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ ls
 echo " Press Yes[Y] to continue coreos installation into machine, No[N] to exit " flag
 # to read strings
 read flag
-if (( (( $flag == "Y")) || (($flag == "y")) )) ]
+if (( (( $flag == "Y")) || (($flag == "y")) )) 
 then
   echo "Starting coreos installation proccess"
   sudo coreos-install -d /dev/sda -C stable -c cloud-config-file.yml
+else
+  echo "you are exited from installation"
 fi
-  
